76eb4e9c5bb6396684c3c95293bc5a378c530ddb
marko bajic <marko@Big-Mac.home>
2016-05-23 21:40:05 -0400

    working

---
+++ test/reg.js
@@ -0,0 +1,23 @@
+var reg =[]
+let c = 0;
+reg[c++] = /\[[0-9]\]+/g;
+reg[c++]= /[\[0-9\]]+$/g;
+//reg[2] = /^[^\[\]()]/g;
+//reg[c++] = /\[[0-9]\]/g;
+reg[c++] = /(\[[0-9]\])*$/g;
+
+var str = 'sso[1]me[1][2][3]';
+
+//var split = str.split(regex);
+//var match = str.match(regex);
+
+for (let i = 0; i <  reg.length; i++) {
+	const s = str.split(reg[i]);
+	const m = str.match(reg[i]);
+	console.log(s + m);
+	console.log(m);
+	console.log('###############');
+}
+
+//console.log(match);
+//console.log(split);
